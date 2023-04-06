338a816390124298b3cc2f263fc76dd218dbb82f
victor-sp-01 <55814035+victor-sp-01@users.noreply.github.com>
2023-04-05 19:12:35 -0500

    Delete js directory

--- js/helpers/ChangeInputValue.js
+++
@@ -1,8 +0,0 @@
-const ChangeInputValue =( element = '', datas = {} )=>{ 
-
-    for( const data in datas )
-        if( element[ data ] ) 
-            element[ data ].value = datas[ data ]
- 
-}
-export default ChangeInputValue
